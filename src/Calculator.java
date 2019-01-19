dea14240c623d931aab9a35b9bcb6856af19340a
sayazh <43455459+sayazh@users.noreply.github.com>
2019-01-19 16:00:39 -0600

    DivideMethod

--- src/Calculator.java
+++ src/Calculator.java
@@ -11,4 +11,7 @@ public class Calculator {
     public static int multiply(int num1, int num2){
         return num1*num2;
 }
+    public static int division(int num1, int num2){
+        return num1/num2;
+}
 }
